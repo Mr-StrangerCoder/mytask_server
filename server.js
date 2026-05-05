062dfe64d60127ec333e6b88825c94e20b744d7e
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-05-05 20:36:01 +0530

    create databses connection

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const connectDB = require('./config/db');
 // const authRoutes = require('./routes/authRoutes');
 // const taskRoutes = require('./routes/taskRoutes');
 // const categoryRoutes = require('./routes/categoryRoutes');
 
 const app = express();
+
+connectDB(); 
 
 app.use(cors());
 app.use(express.json());
@@ -16,13 +18,6 @@ app.use(express.json());
 // app.use('/api/tasks', taskRoutes);
 // app.use('/api/categories', categoryRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected successfully');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log('MongoDB connection error:', err);
-  });
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
