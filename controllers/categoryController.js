08c7234a23f70d12734f1b000eedde8b899e2c44
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-05-05 20:53:39 +0530

    authRoutes add

---
+++ controllers/categoryController.js
@@ -0,0 +1,53 @@
+const Category = require('../models/Category');
+
+// Create Category
+exports.createCategory = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const existingCategory = await Category.findOne({
+      name: { $regex: new RegExp(`^${name}$`, 'i') },
+      user: req.user.id
+    });
+
+    if (existingCategory) {
+      return res.status(400).json({ message: 'Category already exists' });
+    }
+
+    const category = await Category.create({
+      name,
+      user: req.user.id
+    });
+
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Get All Categories
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find({ user: req.user.id }).sort({ createdAt: -1 });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Delete Category
+exports.deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findOneAndDelete({
+      _id: req.params.id,
+      user: req.user.id
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    res.json({ message: 'Category deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
---
+++ controllers/taskController.js
@@ -0,0 +1,89 @@
+const Task = require('../models/Task');
+
+// Create Task
+exports.createTask = async (req, res) => {
+  const { title, description, dueDate, priority, category } = req.body;
+  try {
+    const task = await Task.create({
+      title,
+      description,
+      dueDate,
+      priority,
+      category,
+      user: req.user.id
+    });
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Get All Tasks
+exports.getTasks = async (req, res) => {
+  const { status, category } = req.query;
+  const filter = { user: req.user.id };
+
+  if (status) filter.status = status;
+  if (category) filter.category = category;
+
+  try {
+    const tasks = await Task.find(filter)
+      .populate('category', 'name')
+      .sort({ createdAt: -1 });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Get Task By ID
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      user: req.user.id
+    }).populate('category', 'name');
+
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Update Task
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.id },
+      req.body,
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Delete Task
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      user: req.user.id
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json({ message: 'Task deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
